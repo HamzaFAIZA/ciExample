352b70c65740f4d3c3f00d6f4e6616e500b56f78
HamzaFAIZA <hamza.faiza@esprit.tn>
2018-10-16 11:05:43 +0200

    first commit

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $tn.esprit.hamzaanis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
